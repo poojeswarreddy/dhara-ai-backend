11e827104c58e5ce22a516aea7dda75456021b55
pilli poojeswarreddy <218x1a4526@khitguntur.ac.in>
2026-03-04 01:23:38 +0530

    Set python 3.10 and enable tensorflow

--- api_server.py
+++ api_server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
