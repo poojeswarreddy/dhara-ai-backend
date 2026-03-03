a47a3f37fff66f645dd54bfcd2cde4fcf129d39a
pilli poojeswarreddy <218x1a4526@khitguntur.ac.in>
2026-03-04 01:13:26 +0530

    Remove tensorflow for cloud deploy

--- api_server.py
+++ api_server.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
